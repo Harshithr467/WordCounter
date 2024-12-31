3261d899f258e3400c950a33cdecf3dd8e39d6b2
Harshithr467 <Harshith.revuru@gmail.com>
2024-12-31 14:24:09 -0500

    update

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,4 +1,20 @@
+/**
+ * This program asks the user for a text file and makes a
+ * HTML file that contains a table which lists all the words(in
+ * alphabetical Order) that occurred and its count(the number of time that word has
+ * occurred in the text file.
+ * 
+ * @author Harshith Revuru
+ */
 public class WordCounter {
 
-    
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private WordCounter() {
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
