51737eb412c3442e9d54a6eb4ab455a76c5b6780
Harshithr467 <Harshith.revuru@gmail.com>
2024-12-31 16:21:18 -0500

    Updated Main Method

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,4 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Comparator;
+import java.util.Scanner;
 
 /**
  * This program asks the user for a text file and makes a
@@ -26,9 +33,53 @@ public class WordCounter {
             return s1.toLowerCase().compareTo(s2.toLowerCase());
         }
     }
+
     
 
     public static void main(String[] args) {
+        Scanner snc = new Scanner(System.in);
+
+        System.out.print("Enter the name of the Text file(with location): ");
+        String txtFile = snc.nextLine();
+
+        System.out.print("Enter the name of the HTML file(with location folder): ");
+        String htmlFile = snc.nextLine();
+
+        BufferedReader input = null;
+        PrintWriter output = null;
+
+        try {
+            // Open input file for reading.
+            input = new BufferedReader(new FileReader(txtFile));
+        } catch (IOException e) {
+            System.err.println("Error reading from file: " + txtFile);
+            e.printStackTrace();
+            snc.close();
+            return;
+        } finally { // Close input stream
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    System.err.println("Error closing input stream.");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try {
+            // Open output file for writing.
+            output = new PrintWriter(new BufferedWriter(new FileWriter(htmlFile)));
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + htmlFile);
+            e.printStackTrace();
+            //e.getMessage();
+        } finally { // Close output stream
+            if (output != null) {
+                output.close();
+            }
+        }
         
+        snc.close();
     }
 }
